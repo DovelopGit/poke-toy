0d114ce9fc17714acdb59320337041cbdd8b2547
rojojun <zoloman316@gmail.com>
2023-06-30 16:55:36 +0900

    first commit

---
+++ src/main/java/com/toy/poketoy/PokeToyApplication.java
@@ -0,0 +1,13 @@
+package com.toy.poketoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/poketoy/PokeToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.poketoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
