30bbab103bb893607a7856ea3524431552b655d2
rojojun <zoloman316@gmail.com>
2023-07-03 22:02:24 +0900

    QueryDSL 설정

--- src/main/java/com/toy/poketoy/entry/entity/Entry.java
+++ src/main/java/com/toy/poketoy/entry/entity/Entry.java
@@ -1,4 +1,21 @@
 package com.toy.poketoy.entry.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
 public class Entry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 }
