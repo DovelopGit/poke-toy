d2e65fff60dc8c4b3baad5c9052a70064579d130
rojojun <zoloman316@gmail.com>
2023-07-05 20:29:41 +0900

    enumType 설정

--- src/main/java/com/toy/poketoy/monster/entity/MonsterType.java
+++ src/main/java/com/toy/poketoy/monster/entity/MonsterType.java
@@ -1,16 +1,30 @@
 package com.toy.poketoy.monster.entity;
 
 public enum MonsterType {
-    SPECIAL("특수"),
-    PHYSICAL("물리");
-
-    public String getName() {
-        return name;
-    }
+    NORMAL("노말"),
+    FIRE("불꽃"),
+    WATER("물"),
+    GRASS("풀"),
+    ELECTRICITY("전기"),
+    ICE("얼음"),
+    FIGHTER("격투"),
+    POISON("독"),
+    GROUND("땅"),
+    FLY("비행"),
+    ESPER("에스퍼"),
+    BUG("벌레"),
+    ROCK("바위"),
+    GHOST("고스트"),
+    DRAGON("드래곤")
+    ;
 
     MonsterType(String name) {
         this.name = name;
     }
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
 }
